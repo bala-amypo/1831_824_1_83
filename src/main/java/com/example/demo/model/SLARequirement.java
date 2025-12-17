3d88530bcec3595fdf60fc35eea527c575822c47
bala-amypo <bala@amypo.in>
2025-12-17 09:10:17 +0000

    User commit

--- src/main/java/com/example/demo/model/SLARequirement.java
+++ src/main/java/com/example/demo/model/SLARequirement.java
@@ -1,59 +1,59 @@
-// package com.example.demo.model;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.Column;
-// import jakarta.validation.constraints.Size;
+package com.example.demo.model;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Size;
 
-// public class SLARequirement{
-//     @Id
-//     private Long id;
-// @Column(unique=true)
-// private String requirementName;
-// private String description;
+public class SLARequirement{
+    @Id
+    private Long id;
+@Column(unique=true)
+private String requirementName;
+private String description;
 
-// private int maxDeliveryDays;
-// @Size(min=0,max=100)
-// private double minQualityScore;
-// private boolean active;
+private int maxDeliveryDays;
+@Size(min=0,max=100)
+private double minQualityScore;
+private boolean active;
 
-// public Long getId(){
-//     return id;
-// }
-// public void setId(Long id){
-//     this.id=id;
-// }
+public Long getId(){
+    return id;
+}
+public void setId(Long id){
+    this.id=id;
+}
 
-// public String getId(){
-//     return requirementName;
-// }
-// public void setId(String requirementName){
-//     this.requirementName=requirementName;
-// }
+public String getRequirementName(){
+    return requirementName;
+}
+public void setRequiredName(String requirementName){
+    this.requirementName=requirementName;
+}
 
-// public String getId(){
-//     return description;
-// }
-// public void setId(String description){
-//     this.description=description;
-// }
+public String getId(){
+    return description;
+}
+public void setId(String description){
+    this.description=description;
+}
 
-// public int getId(){
-//     return maxDeliveryDays;
-// }
-// public void setId(int maxDeliveryDays){
-//     this.maxDeliveryDays=maxDeliveryDays;
-// }
+public int getId(){
+    return maxDeliveryDays;
+}
+public void setId(int maxDeliveryDays){
+    this.maxDeliveryDays=maxDeliveryDays;
+}
 
-// public double getId(){
-//     return minQualityScore;
-// }
-// public void setId(double minQualityScore){
-//     this.minQualityScore=minQualityScore;
-// }
+public double getId(){
+    return minQualityScore;
+}
+public void setId(double minQualityScore){
+    this.minQualityScore=minQualityScore;
+}
 
-// public boolean getId(){
-//     return active;
-// }
-// public void setId(boolean active){
-//     this.active=active;
-// }
-// }
+public boolean getId(){
+    return active;
+}
+public void setId(boolean active){
+    this.active=active;
+}
+}
