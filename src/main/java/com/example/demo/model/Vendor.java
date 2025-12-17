3652f9fe45af6832b315dd6976553799991bc115
bala-amypo <bala@amypo.in>
2025-12-17 05:18:06 +0000

    User commit

--- src/main/java/com/example/demo/model/Vendor.java
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -4,4 +4,14 @@ public class Vendor{
     @Id
     private Long id;
 @column(unique=true)
+private String requirementName;
+private String description;
+private int maxDeliveryDays;
+private double minQualityScore;
+private boolean active;
+
+public Long getId(){
+    return id;
+}
+public 
 }
