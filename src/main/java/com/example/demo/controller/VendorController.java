2e798729424e40dbc6d808a3fd756aaa23c61389
bala-amypo <bala@amypo.in>
2025-12-18 10:35:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/VendorController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Vendor;
+import com.example.demo.service.VendorService;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vendors")
+public class VendorController {
+
+    @Autowired
+    private VendorService service;
+
+    
+    @PostMapping
+    public Vendor createVendor(@RequestBody Vendor vendor) {
+        return service.createVendor(vendor);
+    }
+
+
+    @GetMapping("/{id}")
+    public Vendor getVendor(@PathVariable Long id) {
+        return service.getVendorById(id);
+    }
+
+    
+    @GetMapping
+    public List<Vendor> getAllVendors() {
+        return service.getAllVendors();
+    }
+
+    
+    @PutMapping("/{id}")
+    public Vendor updateVendor(
+            @PathVariable Long id,
+            @RequestBody Vendor vendor) {
+        return service.updateVendor(id, vendor);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deactivateVendor(@PathVariable Long id) {
+        service.deactivateVendor(id);
+    }
+}
