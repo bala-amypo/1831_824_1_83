9dd38587ef05bc23987f8360119e15f6cde22daa
bala-amypo <bala@amypo.in>
2025-12-18 03:44:25 +0000

    User commit

--- src/main/java/com/example/demo/model/DeliveryEvaluation.java
+++ src/main/java/com/example/demo/model/DeliveryEvaluation.java
@@ -1,2 +1,10 @@
-// package com.example.demo.model;
-// import jakarta.persistence.Entity;
+package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class DeliveryEvaluation{
+    @Id
+    private Long id;
+    private int actualDeliveryDays;
+    private double qualityScore
+}
