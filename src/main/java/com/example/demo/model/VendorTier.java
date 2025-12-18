fad9323e7d4c9d614b911f8d0462b03ea2f08a6f
bala-amypo <bala@amypo.in>
2025-12-18 05:29:25 +0000

    User commit

--- src/main/java/com/example/demo/model/VendorTier.java
+++ src/main/java/com/example/demo/model/VendorTier.java
@@ -4,3 +4,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.validation.constraints.Size;
+import jakarta.persistence.Column;
+@Entity
+public class VendorTier{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+     @Column(unique=true)
+     private String tierName;
+     private double minScoreThreshold;
+     private string description;
+     private boolean active = true;
+     
+}
