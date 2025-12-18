422c60559b295bd57137d7880d9571000d26b27c
bala-amypo <bala@amypo.in>
2025-12-18 13:40:07 +0000

    User commit

--- src/main/java/com/example/demo/service/VendorService.java
+++ src/main/java/com/example/demo/service/VendorService.java
@@ -1,8 +1,7 @@
 package com.example.demo.service;
 import java.util.List;
-import org.springframework.stereotype.Service;
 import com.example.demo.model.Vendor;
-@Service
+
 public interface VendorService{
     Vendor createVendor(Vendor vendor);
     Vendor updateVendor(Long id, Vendor vendor);
