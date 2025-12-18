cc7fe141472acfa9fe1b4e224060aa47034b68a7
bala-amypo <bala@amypo.in>
2025-12-18 08:40:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/DeliveryEvaluationService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+import java.util.List;
+import com.example.demo.model.DeliveryEvaluation;
+public interface .DeliveryEvaluationService{
+ 
+    DeliveryEvaluation createEvaluation(DeliveryEvaluation evaluation);
+
+    DeliveryEvaluation getEvaluationById(Long id);
+    List<DeliveryEvaluation> getEvaluationsForVendor(Long vendorId);
+    List<DeliveryEvaluation> getEvaluationsForRequirement(Long requirementId);
+}
--- src/main/java/com/example/demo/service/SLARequirementService.java
+++ src/main/java/com/example/demo/service/SLARequirementService.java
@@ -1,10 +1,10 @@
 package com.example.demo.service;
 import java.util.List;
-import com.example.demo.model.SLARequirementS;
-public interface VendorService{
-    Vendor createVendor(Vendor vendor);
-    Vendor updateVendor(Long id, Vendor vendor);
-    Vendor getVendorById(Long id);
-    List<Vendor> getAllVendors();
-    void deactivateVendor(Long id);
+import com.example.demo.model.SLARequirementService;
+public interface SLARequirementService{
+    SLARequirement createRequirement(SLARequirement req);
+    SLARequirement updateRequirement(Long id, SLARequirement req);
+    SLARequirement getRequirementById(Long id);
+    List<SLARequirement> getAllRequirements();
+    void deactivateRequirement(Long id);
 }
