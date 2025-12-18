b7bc4a1565c525bd9e51e76b82b719690f2e7134
bala-amypo <bala@amypo.in>
2025-12-18 15:52:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeliveryEvaluationRepository.java
+++ src/main/java/com/example/demo/repository/DeliveryEvaluationRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.DeliveryEvaluation;
 
-import java.time.LocalDate; // <-- Add this import
+import java.time.LocalDate;
 
 @Repository
 public interface DeliveryEvaluationRepository extends JpaRepository<DeliveryEvaluation, Long> {
