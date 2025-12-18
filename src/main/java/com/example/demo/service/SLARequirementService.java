5b69b4b255bcd86e2292279168d5157218c22dfc
bala-amypo <bala@amypo.in>
2025-12-18 09:10:51 +0000

    User commit

--- src/main/java/com/example/demo/service/SLARequirementService.java
+++ src/main/java/com/example/demo/service/SLARequirementService.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 import java.util.List;
-import com.example.demo.model.SLARequirementService;
+import com.example.demo.model.SLARequirement;
 public interface SLARequirementService{
     SLARequirement createRequirement(SLARequirement req);
     SLARequirement updateRequirement(Long id, SLARequirement req);
--- src/main/java/com/example/demo/service/VendorTierService.java
+++ src/main/java/com/example/demo/service/VendorTierService.java
@@ -1,7 +1,7 @@
 package com.example.demo.service;
 import java.util.List;
 import com.example.demo.model.VendorTier;
-public interface .VendorTierService{
+public interface VendorTierService{
      VendorTier createTier(VendorTier tier);
      VendorTier updateTier(Long id, VendorTier tier);
      VendorTier getTierById(Long id);
