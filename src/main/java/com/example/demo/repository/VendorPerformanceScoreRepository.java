14611a3bc0d0cb73fd3414bf40f773d0f47a4372
bala-amypo <bala@amypo.in>
2025-12-18 06:23:04 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/VendorPerformanceScoreRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.VendorPerformanceScore;
+public interface VendorPerformanceScoreRepository extends JpaRepository<VendorPerformanceScore,Long>{
+    
+}
