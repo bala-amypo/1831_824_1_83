c732bb7826031cf04397a92c50a8775cc2eed368
bala-amypo <bala@amypo.in>
2025-12-18 03:54:25 +0000

    User commit

--- src/main/java/com/example/demo/model/DeliveryEvaluation.java
+++ src/main/java/com/example/demo/model/DeliveryEvaluation.java
@@ -1,6 +1,7 @@
 package com.example.demo.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import java.util.Date;
 @Entity
 public class DeliveryEvaluation{
     @Id
@@ -10,4 +11,53 @@ public class DeliveryEvaluation{
     private Date evaluationDate;
     private boolean meetsDeliveryTarget;
     private boolean meetsQualityTarget;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getActualDeliveryDays() {
+        return actualDeliveryDays;
+    }
+
+    public void setActualDeliveryDays(int actualDeliveryDays) {
+        this.actualDeliveryDays = actualDeliveryDays;
+    }
+
+    public double getQualityScore() {
+        return qualityScore;
+    }
+
+    public void setQualityScore(double qualityScore) {
+        this.qualityScore = qualityScore;
+    }
+
+    public Date getEvaluationDate() {
+        return evaluationDate;
+    }
+
+    public void setEvaluationDate(Date evaluationDate) {
+        this.evaluationDate = evaluationDate;
+    }
+
+    public boolean isMeetsDeliveryTarget() {
+        return meetsDeliveryTarget;
+    }
+
+    public void setMeetsDeliveryTarget(boolean meetsDeliveryTarget) {
+        this.meetsDeliveryTarget = meetsDeliveryTarget;
+    }
+
+    public boolean isMeetsQualityTarget() {
+        return meetsQualityTarget;
+    }
+
+    public void setMeetsQualityTarget(boolean meetsQualityTarget) {
+        this.meetsQualityTarget = meetsQualityTarget;
+    }
+    public DeliveryEvaluation()
 }
