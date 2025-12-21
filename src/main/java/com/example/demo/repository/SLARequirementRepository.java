b167a9fbd461a5ed413e427e7aef89f375556b58
bala-amypo <bala@amypo.in>
2025-12-21 17:22:37 +0000

    User commit

--- src/main/java/com/example/demo/repository/SLARequirementRepository.java
+++ src/main/java/com/example/demo/repository/SLARequirementRepository.java
@@ -1,9 +1,7 @@
 package com.example.demo.repository;
-
 import com.example.demo.model.SLARequirement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
-
 public interface SLARequirementRepository extends JpaRepository<SLARequirement, Long> {
     Optional<SLARequirement> findByRequirementName(String requirementName);
 }
