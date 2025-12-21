dab979a9377668d689da55c0cc0992b31c282eec
bala-amypo <bala@amypo.in>
2025-12-21 12:08:15 +0000

    User commit

--- src/main/java/com/example/demo/service/VendorPerformanceScoreService.java
+++ src/main/java/com/example/demo/service/VendorPerformanceScoreService.java
@@ -1,8 +1,8 @@
-package com.example.demo.service;
-import java.util.List;
-import com.example.demo.model.VendorPerformanceScore;
-public interface VendorPerformanceScoreService{
-   VendorPerformanceScore calculateScore(Long vendorId);
-   VendorPerformanceScore getLatestScore(Long vendorId);
-   List<VendorPerformanceScore> getScoresForVendor(Long vendorId);
-}
+// package com.example.demo.service;
+// import java.util.List;
+// import com.example.demo.model.VendorPerformanceScore;
+// public interface VendorPerformanceScoreService{
+//    VendorPerformanceScore calculateScore(Long vendorId);
+//    VendorPerformanceScore getLatestScore(Long vendorId);
+//    List<VendorPerformanceScore> getScoresForVendor(Long vendorId);
+// }
