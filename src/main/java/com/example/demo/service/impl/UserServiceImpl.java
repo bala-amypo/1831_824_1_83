a4a56385c87708f22879a1b8af85ef7fa10e5f92
bala-amypo <bala@amypo.in>
2025-12-26 09:57:53 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -74,13 +74,13 @@
 //                 .orElseThrow(() -> new IllegalArgumentException("not found"));
 //     }
 // }
-
 package com.example.demo.service.impl;
 
-import com.example.demo.model.User;
 import com.example.demo.security.JwtTokenProvider;
 import com.example.demo.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     private final JwtTokenProvider jwtTokenProvider;
@@ -91,7 +91,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User register(User user) {
-        return user; // tests don’t validate persistence
+        return user;
     }
 
     @Override
